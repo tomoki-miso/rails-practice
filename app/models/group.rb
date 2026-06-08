2e594b3c1fad6235d1a8f536d7110a939208ea85
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-06-08 16:57:09 +0900

    feat:  削除時カスケード設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :rounds
-  has_many :comments
-  has_many :group_members
-  has_many :users, through: :group_members
+  has_many :comments, dependent: :destryoy
+  has_many :group_members, dependent: :destryoy
+  has_many :users, through: :group_members, dependent: :destryoy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :group_members
-  has_many :preparation_completions
-  has_many :groups, through: :group_members
+  has_many :group_members, dependent: :destryoy
+  has_many :preparation_completions, dependent: :destryoy
+  has_many :groups, through: :group_members, dependent: :destryoy
   has_many :comments
 end
