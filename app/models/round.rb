1065b1eec01aabd20acd1aca6feba2dc59e354d1
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-06-10 03:15:42 +0000

    feat: Roundモデルにバリデーションを追加

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,19 @@
 class Round < ApplicationRecord
   belongs_to :group
   has_many :preparation_completions, dependent: :destroy
+
+  validates :number, uniqueness: { scope: :group_id }
+  validates :number, presence: true
+  validates :start_page, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :end_page, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  validate :start_page_must_be_less_than_or_equal_to_end_page
+
+  private
+
+  def start_page_must_be_less_than_or_equal_to_end_page
+    return if start_page.blank? || end_page.blank?
+    return if start_page <= end_page
+    errors.add("開始ページは終了ページ以下にしてください")
+  end
 end
