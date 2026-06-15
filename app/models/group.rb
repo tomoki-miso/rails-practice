11a4b603fec80bca8a23fc8c15667dd2e6737241
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-06-15 07:13:48 +0000

    feat: コメントモデルにポリモーフィック関連を追加し、バリデーションとスコープを実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,9 @@
 class Group < ApplicationRecord
   has_many :rounds
-  has_many :comments, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+
   has_many :group_members, dependent: :destroy
   has_many :users, through: :group_members, dependent: :destroy
   has_one_attached :pdf
+  scope :top_level, -> { where(reply_comment_id: nil) }
 end
