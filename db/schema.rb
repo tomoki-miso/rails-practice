1dd8fe4c0dfe9d63595e22f81e3d401513e7d0b9
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-06-15 10:02:43 +0000

    feat: グループモデルにページ数カラムを追加し、スキーマを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_10_070052) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_095832) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -70,6 +70,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_10_070052) do
     t.datetime "created_at", null: false
     t.string "description"
     t.string "file_path"
+    t.integer "pages"
     t.string "title", null: false
     t.datetime "updated_at", null: false
   end
